2ba16150975f6e6220d35d2fa5731a1666a52539
Aayush <aayush19saxena@gmail.com>
2018-01-23 13:02:27 -0800

    Pre testing- Added complex computations

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -10,6 +10,7 @@ import Foundation
 
 // All your work will go in here
 class Calculator {
+    
     public func add(_ args: [Int]) -> Int {
         var sum = 0
         for arg in args {
@@ -57,6 +58,42 @@ class Calculator {
     
     public func subtract(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
         return (lhs.0 - rhs.0, lhs.1 - rhs.1)
+    }
+    
+    public func mathOp(lhs: Int, rhs: Int, op: (Int, Int) -> Int) -> Int {
+        return op(lhs, rhs)
+    }
+    
+    public func mathOp(args: [Int], beg: Int, op: (Int, Int) -> Int) ->Int {
+        var total = beg
+        for arg in args {
+            total = op(total, arg)
+        }
+        return total
+    }
+    
+    public func add(lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
+        var emptyDict: [String: Int] = [:]
+        if !lhs.keys.elementsEqual(rhs.keys) {
+            return ["The dictionary keys do not match": 0]
+        }
+        for key in lhs.keys {
+            emptyDict[key] = lhs[key]! + rhs[key]!
+        }
+        
+        return emptyDict
+    }
+    
+    public func subtract(lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
+        var emptyDict: [String: Int] = [:]
+        if !lhs.keys.elementsEqual(rhs.keys) {
+            return ["The dictionary keys do not match": 0]
+        }
+        for key in lhs.keys {
+            emptyDict[key] = lhs[key]! - rhs[key]!
+        }
+        
+        return emptyDict
     }
     
 }
